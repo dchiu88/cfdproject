a2c9e1e5835c45b901956cad4e62ae2314c9014f
dchiu88 <davidwchiu@gmail.com>
2020-01-18 11:20:08 -0500

    First commit

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,4 @@
 class Admin < ApplicationRecord
+  has_many :employees
+  has_many :timesheetreports
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :timesheetreports
-  resources :timesheet_entries
+  resources :timesheetreports do
+    resources :timesheet_entries
+  end
   resources :employees
   resources :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
