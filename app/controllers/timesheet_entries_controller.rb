02827dd05545639c400802f5bf81b6c3a982d8c5
dchiu88 <davidwchiu@gmail.com>
2020-02-08 15:30:42 -0500

    Created form for timesheet entries

--- app/controllers/timesheet_entries_controller.rb
+++ app/controllers/timesheet_entries_controller.rb
@@ -1,2 +1,26 @@
 class TimesheetEntriesController < ApplicationController
+
+def index
+end
+
+def show
+end
+
+def new
+    @timesheet_entry = TimesheetEntry.new
+end
+
+def create
+  @timesheet_entry.new(timesheet_entry_params)
+  if @timesheet_entry.save
+    redirect_to timesheet_entry_path(@timesheet_entry)
+end
+
+private
+
+def timesheet_entry_params
+  params.require(:timesheet_entry).permit(:hours, :pto)
+  end
+end
+
 end
