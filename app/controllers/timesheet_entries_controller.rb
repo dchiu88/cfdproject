20369bbd63a86afbded931758eabea93c3652384
dchiu88 <davidwchiu@gmail.com>
2020-05-09 17:17:31 -0400

    attempted to configure show page for timesheet entries

--- app/controllers/timesheet_entries_controller.rb
+++ app/controllers/timesheet_entries_controller.rb
@@ -4,6 +4,10 @@ def index
 end
 
 def show
+  @timesheet_entry = TimesheetEntry.find_by(id: params[:id])
+    if !@timesheet_entry
+      redirect_to timesheet_entry_path(@timesheet_entry)
+    end
 end
 
 def new
@@ -11,16 +15,15 @@ def new
 end
 
 def create
-  @timesheet_entry.new(timesheet_entry_params)
+  @timesheet_entry = TimesheetEntry.new(timesheet_entry_params)
   if @timesheet_entry.save
     redirect_to timesheet_entry_path(@timesheet_entry)
+  end
 end
 
 private
 
 def timesheet_entry_params
   params.require(:timesheet_entry).permit(:hours, :pto)
-  end
 end
-
 end
--- app/controllers/timesheetreports_controller.rb
+++ app/controllers/timesheetreports_controller.rb
@@ -1,2 +1,29 @@
 class TimesheetreportsController < ApplicationController
+
+def index
+end
+
+def show
+  #enter logic here to find related time sheet entries by periodend
+  # @timesheet_entry.where(timesheet_entry :date > timesheetreport :startdate and timesheet_entry < enddate) 
+end
+
+def new
+  @timesheetreport = Timesheetreport.new
+end
+
+def create
+  @timesheetreport = Timesheetreport.new(timesheetreport_params)
+  if @timesheetreport.save
+    redirect_to timesheetreport_path(@timesheetreport)
+  end
+end
+
+
+private
+
+def timesheetreport_params
+  params.require(:timesheetreport).permit(:hours, :pto)
+end
+
 end
